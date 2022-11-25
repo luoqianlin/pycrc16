c9c9115c215ea36ac0c49789d5816549e9ea3636
luo <qianlinluo@foxmail.com>
2022-11-25 11:21:23 +0800

    fix 'DeprecationWarning: PY_SSIZE_T_CLEAN will be required for '#' formats'

--- src/_crc16module.c
+++ src/_crc16module.c
@@ -18,7 +18,7 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * **************************************************************************/
-
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
 /* table for calculating CRC
